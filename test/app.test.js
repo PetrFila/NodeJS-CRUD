c2a24ad3a3e5f2abfbb2ef20e8821d522c0f15aa
Petr Fila <pet.fila@gmail.com>
2018-02-01 00:34:00 +1100

    testing environment setup

--- test/app.test.js
+++ test/app.test.js
@@ -1,4 +1,8 @@
 const knex = require('../db/knex');
+const request = require('supertest');
+const expect = require('chai').expect;
+const app = require('../app')
+const fixtures = require('./fixtures')
 
 describe('CRUD Stickers', () => {
   before((done) => {
@@ -11,7 +15,16 @@ describe('CRUD Stickers', () => {
 
   });
 
-  it('Works...', () => {
-    console.log("It's working!")
+  it('Displays all stickers', (done) => {
+    request(app)
+      .get('/api/v1/stickers')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then((response) => {
+        expect(response.body).to.be.a('array');
+        expect(response.body).to.deep.equal(fixtures.stickers)
+        done();
+      })
+
   });
 });
